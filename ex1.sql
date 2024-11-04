59afa494a160dd8adbb4c02e9dbdfeee13cccdd1
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-11-03 22:50:45 -0300

    Create ex1.sql

---
+++ ex1.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Biblioteca;
+USE Biblioteca;
+
+CREATE TABLE Autores 
+(
+    id INT PRIMARY KEY,
+    nome VARCHAR(100),
+    nacionalidade VARCHAR(50)
+);
+
+CREATE TABLE Categorias 
+(
+    id INT PRIMARY KEY,
+    descricao VARCHAR(50)
+);
+
+CREATE TABLE Livros 
+(
+    id INT PRIMARY KEY,
+    titulo VARCHAR(100),
+    ano_publicacao YEAR,
+    id_autor INT,
+    id_categoria INT,
+    preco DECIMAL(5,2),
+    FOREIGN KEY (id_autor) REFERENCES Autores(id),
+    FOREIGN KEY (id_categoria) REFERENCES Categorias(id)
+);
